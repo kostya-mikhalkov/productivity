fbb6df63cc7a4bf9bb6c9549bb5b6456c3f9f174
Kostya <kostya.mikhalkov@bk.ru>
2022-08-07 18:48:14 +0300

    first step

---
+++ dist/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slider__info').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        centerMode: true,
+        variableWidth: true
+    });
+  });
