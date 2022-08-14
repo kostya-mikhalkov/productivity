b235e4849bc659475299bd14d2d9c754d16f0a65
Kostya <kostya.mikhalkov@bk.ru>
2022-08-14 18:05:14 +0300

    section about

--- dist/js/script.js
+++ dist/js/script.js
@@ -5,6 +5,17 @@ $(document).ready(function(){
         speed: 300,
         slidesToShow: 1,
         centerMode: true,
-        variableWidth: true
+        variableWidth: true,
+        arrows: false
+    });
+    const hamburger = document.querySelector('.hamburger'),
+          menu = document.querySelector('.menu'),
+          close = document.querySelector('.menu__close');
+
+    hamburger.addEventListener('click', () => {
+        menu.classList.add('active');
+    });
+    close.addEventListener('click', () => {
+        menu.classList.remove('active');
     });
   });
