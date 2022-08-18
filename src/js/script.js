8f3330479f3185ab88adcbf34556caa7b0f82ef9
Kostya <kostya.mikhalkov@bk.ru>
2022-08-18 19:05:48 +0300

    slider

--- src/js/script.js
+++ src/js/script.js
@@ -8,6 +8,17 @@ $(document).ready(function(){
         variableWidth: true,
         arrows: false
     });
+    $('.blog__slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        centerMode: true,
+        variableWidth: true,
+        arrows: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>'
+    });
     const hamburger = document.querySelector('.hamburger'),
           menu = document.querySelector('.menu'),
           close = document.querySelector('.menu__close');
